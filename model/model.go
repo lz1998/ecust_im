7673582c84ceea9effaac33e665945c790ad8584
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-12-22 05:49:05 +0800

    load password from env

--- model/model.go
+++ model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
@@ -9,7 +11,8 @@ import (
 var Db *gorm.DB
 
 func init() {
-	dsn := "im:xxx@tcp(tmp.lz1998.xin:13306)/im?charset=utf8mb4&parseTime=True&loc=Local" // TODO gitignore 防止密码泄露
+	pass := os.Getenv("MYSQL_PASSWORD")
+	dsn := "im:" + pass + "@tcp(tmp.lz1998.xin:13306)/im?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true,
