1ee0c5a0092ffc1283af0b3852a09cb2a53360be
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-12-22 07:35:11 +0800

    getUserInfo, setUserInfo

--- handler/account.go
+++ handler/account.go
@@ -23,7 +23,7 @@ func Register(c *gin.Context) {
 	resp := &dto.RegisterResp{
 		Ok:       true,
 		Msg:      "succeed to register",
-		UserInfo: ConvertUserModelToProto(u),
+		UserInfo: ConvertUserModelToProto(u, true),
 	}
 	Return(c, resp)
 }
@@ -58,19 +58,71 @@ func Login(c *gin.Context) {
 }
 
 func SetUserInfo(c *gin.Context) {
-
-
-
+	req := &dto.SetUserInfoReq{}
+	if err := c.Bind(req); err != nil {
+		c.String(http.StatusBadRequest, "bad request, not protobuf")
+		return
+	}
+	if err := user.UpdateUser(ConvertUsersProtoToModel(req.UserInfos)); err != nil {
+		c.String(http.StatusInternalServerError, "update error")
+		return
+	}
+	resp := &dto.SetUserInfoResp{}
+	Return(c, resp)
 }
 
 func GetUserInfo(c *gin.Context) {
-
+	req := &dto.GetUserInfoReq{}
+	if err := c.Bind(req); err != nil {
+		c.String(http.StatusBadRequest, "bad request, not protobuf")
+		return
+	}
+	users, err := user.ListUser(req.UserIds)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "list user error")
+		return
+	}
+	resp := &dto.GetFriendsResp{
+		UserInfos: ConvertUsersModelToProto(users, true),
+	}
+	Return(c, resp)
 }
 
-func ConvertUserModelToProto(modelUser *user.EcustUser) *dto.UserInfo {
+func ConvertUserModelToProto(modelUser *user.EcustUser, ignorePassword bool) *dto.UserInfo {
 	return &dto.UserInfo{
-		UserId:   modelUser.UserId,
+		UserId: modelUser.UserId,
+		Password: func() string {
+			if ignorePassword {
+				return ""
+			} else {
+				return modelUser.Password
+			}
+		}(),
 		Nickname: modelUser.Nickname,
-		Email:    "",
+		Email:    "", // TODO 好像没啥用？有空可以做个人信息
 	}
+}
+
+func ConvertUsersModelToProto(modelUsers []*user.EcustUser, ignorePassword bool) []*dto.UserInfo {
+	protoUsers := make([]*dto.UserInfo, 0)
+	for _, modelUser := range modelUsers {
+		protoUsers = append(protoUsers, ConvertUserModelToProto(modelUser, ignorePassword))
+	}
+	return protoUsers
+}
+
+func ConvertUserProtoToModel(protoUser *dto.UserInfo) *user.EcustUser {
+	return &user.EcustUser{
+		UserId:   protoUser.UserId,
+		Password: protoUser.Password,
+		Nickname: protoUser.Nickname,
+	}
+}
+
+func ConvertUsersProtoToModel(protoUsers []*dto.UserInfo) []*user.EcustUser {
+	modelUsers := make([]*user.EcustUser, 0)
+	for _, protoUser := range protoUsers {
+		modelUsers = append(modelUsers, ConvertUserProtoToModel(protoUser))
+	}
+	return modelUsers
 }
